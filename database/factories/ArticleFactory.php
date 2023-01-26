02ababba826452d30b9a3ebbf0c50a3e44a14a5b
MonstrHW <nukolau08@gmail.com>
2023-01-26 18:19:52 +0800

    feat(factory): add fake image for article (#41)

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -3,7 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 /**
@@ -11,6 +11,28 @@ use Illuminate\Support\Str;
  */
 class ArticleFactory extends Factory
 {
+    private function getFakeImage()
+    {
+        $imageUrl = fake()->imageUrl();
+        $imageContent = file_get_contents($imageUrl);
+        $imagePath = 'images/' . uniqid() . '.png';
+
+        $result = Storage::disk('public')->put($imagePath, $imageContent) ? $imagePath : null;
+
+        return $result;
+    }
+
+    public function withFakeImage()
+    {
+        return $this->state(function () {
+            $fakeImage = rand(0, 1) ? $this->getFakeImage() : null;
+
+            return [
+                'image' => $fakeImage,
+            ];
+        });
+    }
+
     /**
      * Define the model's default state.
      *
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Article::factory(3)
+            ->withFakeImage()
             ->hasSections(3)
             ->create();
     }
